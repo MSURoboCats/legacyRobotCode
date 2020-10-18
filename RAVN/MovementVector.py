995a33d8bf1602e0c40c3e95359781d99273abbd
rustky <50724477+rustky@users.noreply.github.com>
2020-10-18 17:48:59 -0600

    Movement vector work

--- RAVN/MovementVector.py
+++ RAVN/MovementVector.py
@@ -1,3 +1,7 @@
-import numpy as np 
-
+import pandas as pd
+df = pd.read_csv('./OILT Output Frames/CurrentFrame1.csv', sep=',', header=None)
+df.columns = ['X Cent', 'Y Cent',	'X Dist', 'Y Dist',	'Class']
+print(df)
 class_priority = [1,2,3,4,5]
+
+print(df.values[1][4])
