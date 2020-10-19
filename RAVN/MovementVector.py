cfe8250d4201067bbe9d0a0a5c25a950a6bffd2c
Scott Smith <smith427@live.com>
2020-10-19 11:31:22 -0600

    add class to RAVN code

--- RAVN/MovementVector.py
+++ RAVN/MovementVector.py
@@ -1,3 +1,28 @@
+FILENAME = "inputs.csv"
+
+import csv
 import numpy as np 
 
+class ObjectOfInterest:
+    def __init__(self, number, cx, cy, dx, dy):
+        self.number = number
+        self.cx = cx
+        self.cy = cy
+        self.dx = dx
+        self.dy = dy
+
+
+def sortCriteria(obj):
+    return obj.number
+
+
+def prioritizeObjects():
+    with open(FILENAME, "r") as frame:
+        reader = csv.reader(frame)
+        for row in reader:
+            objects.append(ObjectOfInterest(row[0], row[1], row[2], row[3], row[4]))
+    objects.sort(key=sortCriteria)
+
+
 class_priority = [1,2,3,4,5]
+objects = []
