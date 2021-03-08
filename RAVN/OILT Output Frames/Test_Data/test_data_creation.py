ed2968536463df3b02422cf86c38d07cad83a1f4
rustky <kylerustxc@gmail.com>
2021-03-07 21:57:33 -0700

    bug fix

--- RAVN/OILT Output Frames/Test_Data/test_data_creation.py
+++ RAVN/OILT Output Frames/Test_Data/test_data_creation.py
@@ -7,14 +7,12 @@ def list_A(tot_objects, num_file):
     rows = rd.randint(4,tot_objects)
     current_view = np.empty([rows,5])
     classes = [*range(1,tot_objects+1)]
-    print(classes)
     for i in range(rows):
-        current_view[i,0] = rd.randint(0,720)
-        current_view[i,1] = rd.randint(0,1280)
-        current_view[i,2] = rd.randint(0,720)
-        current_view[i,3] = rd.randint(0,1280)
+        current_view[i,0] = rd.randint(0,1280)
+        current_view[i,1] = rd.randint(0,720)
+        current_view[i,2] = rd.randint(0,1280)
+        current_view[i,3] = rd.randint(0,720)
         obj_type = rd.choice(classes)
-        print(obj_type)
         current_view[i,4] = obj_type
         classes.remove(obj_type)
     np.savetxt("/Users/kyler/OneDrive/Documents/Capstone/robotCode/RAVN/OILT Output Frames/Test_Data/CurrentFile" + str(num_file) +".csv",current_view,delimiter=",") 
