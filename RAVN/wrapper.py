3e8062ebed2c5ea9fe0c3a25291a72ede2b55fb7
rustky <kylerustxc@gmail.com>
2021-03-16 21:30:03 -0600

    test 1

--- RAVN/wrapper.py
+++ RAVN/wrapper.py
@@ -20,8 +20,25 @@ def test1_2_1():
         file1 = open("/Users/kyler/OneDrive/Documents/Capstone/robotCode/RAVN/OILT Output Frames/Test_Data/List A/Test1_2_1.txt", "a") 
         L = str(target) + "\n"
         file1.writelines(L) 
-        file1.close() 
+        file1.close()
+
+def test1():
+    diff_priorities = [[1,2,3,4,5,6],[2,3,4,5,6,1],[3,4,5,6,1,2],[4,5,6,1,2,3],[5,6,1,2,3,4]]
+    filepath = "C:/Users/kyler/OneDrive/Documents/Capstone/robotCode/RAVN/OILT Output Frames/Test_Data/List A/CurrentFile"
+    for i in range(100):
+        if i % 20 == 0:
+            obj_priority = diff_priorities[int(i/20)].copy()
+        cs.relevant_types = obj_priority.copy()
+        print(cs.relevant_types)
+        ss.INPUT_FILE = filepath + str(i) + ".csv"
+        object_list = ss.get_objects()
+        target = cs.search(object_list)
+        file1 = open("/Users/kyler/OneDrive/Documents/Capstone/robotCode/RAVN/OILT Output Frames/Test_Data/List A/Test1.txt", "a") 
+        L = str(target) + "\n"
+        file1.writelines(L) 
+        file1.close()
 
 if __name__ == '__main__':
     # test_1_1_1()
-    test1_2_1()
+    # test1_2_1()
+    test1()
