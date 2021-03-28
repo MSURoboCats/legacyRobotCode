062dbb26249bbedc306a30aac7e61d12ab85e1d7
Xingzi Xu <35911327+xingzixu017@users.noreply.github.com>
2021-03-28 17:57:40 -0600

    Add files via upload
    upload commented version for verification codes

--- o1_tester_bins.py
+++ o1_tester_bins.py
@@ -1,3 +1,4 @@
+# import packages
 import csv
 import os
 import math
@@ -5,20 +6,20 @@ from statistics import mean
 
 det = []
 img = []
-with open("result_bins.txt") as f:
+with open("result_bins.txt") as f: # import the one txt file that contains all the predicted bounding box information, etc.
     c = csv.reader(f, delimiter=' ', skipinitialspace=True)
     for line in c:
         if line != []:
-            if line[-1] == 'milli-seconds.':
-                det.append(['new_image'])
+            if line[-1] == 'milli-seconds.': # check if we have seen a new image
+                det.append(['new_image']) # mark we are looking at a new image
                 img.append(line)
             if line[0] == 'Bins:':
                 line[1] = line[1].split('\t')[0]
                 line[8] = line[8].split(')')[0]
-                line[2] = float(line[2])/float(640)
-                line[4] = float(line[4])/float(480)
-                line[6] = float(line[6])/float(640)
-                line[8] = float(line[8])/float(480)
+                line[2] = float(line[2])/float(640) # reformat the coordinates, widths, heights into ratio of the image
+                line[4] = float(line[4])/float(480) # reformat the coordinates, widths, heights into ratio of the image
+                line[6] = float(line[6])/float(640) # reformat the coordinates, widths, heights into ratio of the image
+                line[8] = float(line[8])/float(480) # reformat the coordinates, widths, heights into ratio of the image
                 det.append(line)
 # up till here, we have line[2] as top left corner's x coordinate,
 # line[4] as the top left corner's y coordinate,
@@ -26,9 +27,9 @@ with open("result_bins.txt") as f:
 # line[8] as the height,
 # all in ratio numbers
 
-for line in det:
+for line in det: # for each object detected
     if line[0] == 'Bins:':
-        line[2] = line[2]+line[6]/2
+        line[2] = line[2]+line[6]/2 # reformat the information into the same as the ground truth information
         line[4] = line[4]+line[8]/2
         line.remove(line[0])
         line.remove(line[0])
@@ -38,17 +39,17 @@ for line in det:
 
 
 gro = []
-for file in os.listdir("C:/Users/xingz/OneDrive/Desktop/test_bin"):
+for file in os.listdir("C:/Users/xingz/OneDrive/Desktop/test_bin"): # read in each txt file containing bounding box information for one image
     if file.endswith(".txt"):
-        gro.append(['new_image'])
+        gro.append(['new_image']) # mark when we see a new image
         with open(os.path.join("C:\\Users\\xingz\\OneDrive\\Desktop\\test_bin",file)) as f:
             c = csv.reader(f, delimiter=' ', skipinitialspace=True)
             for line in c:
-                gro.append(line[1:])
+                gro.append(line[1:]) # attach the new object information
 # now we have both the ground truth and the detections, in the same format
 
 gro_pro = []
-while ['new_image'] in gro:
+while ['new_image'] in gro: # reformat the information, the each sublist is an image, each subsublist is an object in the image
     gro.remove(gro[gro.index(['new_image'])])
     if ['new_image'] in gro:
         gro_pro.append(gro[0:gro.index(['new_image'])])
@@ -57,7 +58,7 @@ while ['new_image'] in gro:
         gro_pro.append(gro[0:])
 
 det_pro = []
-while ['new_image'] in det:
+while ['new_image'] in det: # reformat the information, the each sublist is an image, each subsublist is an object in the image
     det.remove(det[det.index(['new_image'])])
     if ['new_image'] in det:
         det_pro.append(det[0:det.index(['new_image'])])
@@ -78,18 +79,18 @@ temp_xe = []
 temp_ye = []
 temp_he = []
 temp_we = []
-for i in range(0,199):
-    det_c = det_pro[i]
-    gro_c = gro_pro[i]
+for i in range(0,199): # loop through each image
+    det_c = det_pro[i] # get each image's detected information
+    gro_c = gro_pro[i] # get each image's ground truth information
     det_size = len(det_c) # number of objects in the prediction image
-    for j in range(0,len(gro_c)):
+    for j in range(0,len(gro_c)): # loop through each object in the image
         gro_cc = gro_c[j]
 #            e1 = math.sqrt((det_cc[0]-float(gro_cc[0]))**2+(det_cc[1]-float(gro_cc[1]))**2)/math.sqrt(1)
         for k in range(0,det_size): # for each objects in the prediction image
-            temp_xe.append(abs(float(gro_cc[0])-det_c[k][0]))
-            temp_ye.append(abs(float(gro_cc[1])-det_c[k][1]))
-            temp_we.append(abs(float(gro_cc[2])-det_c[k][2]))
-            temp_he.append(abs(float(gro_cc[3])-det_c[k][3]))
+            temp_xe.append(abs(float(gro_cc[0])-det_c[k][0])) # calculate x coordinate error
+            temp_ye.append(abs(float(gro_cc[1])-det_c[k][1])) # calculate y coordinate error
+            temp_we.append(abs(float(gro_cc[2])-det_c[k][2])) # calculate width error
+            temp_he.append(abs(float(gro_cc[3])-det_c[k][3])) # calculate height error
         e1 = abs(min(temp_xe))
         e4 = abs(max(temp_ye))
         e2 = abs(mean(temp_we))
@@ -105,45 +106,45 @@ for i in range(0,199):
         temp_ye = []
 
 #
-with open('bin_xcoor.txt', 'w') as f:
+with open('bin_xcoor.txt', 'w') as f: # save error result
     for item in xcoor_e:
         f.write("%s\n" % item)
 
-with open('bin_ycoor.txt', 'w') as f:
+with open('bin_ycoor.txt', 'w') as f: # save error result
     for item in ycoor_e:
         f.write("%s\n" % item)
 
-with open('bin_width.txt', 'w') as f:
+with open('bin_width.txt', 'w') as f: # save error result
     for item in width_e:
         f.write("%s\n" % item)
 
-with open('bin_height.txt', 'w') as f:
+with open('bin_height.txt', 'w') as f: # save error result
     for item in height_e:
         f.write("%s\n" % item)
 
-with open('bin_obj.txt', 'w') as f:
+with open('bin_obj.txt', 'w') as f: # save error result
     for item in img:
         f.write("%s\n" % item)
 
 #        
-e_cx_mean = mean(xcoor_e)*100
-e_cy_mean = mean(ycoor_e)*100
-e_w_mean = mean(width_e)*100
-e_h_mean = mean(height_e)*100
+e_cx_mean = mean(xcoor_e)*100 # calculate mean error
+e_cy_mean = mean(ycoor_e)*100 # calculate mean error
+e_w_mean = mean(width_e)*100 # calculate mean error
+e_h_mean = mean(height_e)*100 # calculate mean error
 
-e_cx_max = max(xcoor_e)*100
-e_cy_max = max(ycoor_e)*100
-e_w_max = max(width_e)*100
-e_h_max = max(height_e)*100
+e_cx_max = max(xcoor_e)*100 # calculate max error
+e_cy_max = max(ycoor_e)*100 # calculate max error
+e_w_max = max(width_e)*100 # calculate max error
+e_h_max = max(height_e)*100 # calculate max error
 
-e_cx_min = min(xcoor_e)*100
-e_cy_min = min(ycoor_e)*100
-e_w_min = min(width_e)*100
-e_h_min = min(height_e)*100
+e_cx_min = min(xcoor_e)*100 # calculate min error
+e_cy_min = min(ycoor_e)*100 # calculate min error
+e_w_min = min(width_e)*100 # calculate min error
+e_h_min = min(height_e)*100 # calculate min error
 
 print('The errors for bin detection:')
 
-print('The average x coordinate error is:', e_cx_mean,'%')
+print('The average x coordinate error is:', e_cx_mean,'%') # display the errors
 print('The average y coordinate error is:', e_cy_mean,'%')
 print('The average width error is:', e_w_mean,'%')
 print('The average height error is:', e_h_mean,'%')
--- o2_tester_bins.py
+++ o2_tester_bins.py
@@ -1,3 +1,4 @@
+# import packages
 import csv
 import os
 import math
@@ -5,20 +6,20 @@ from statistics import mean
 
 det = []
 img = []
-with open("result_bins.txt") as f:
+with open("result_bins.txt") as f: # import the one txt file that contains all the predicted bounding box information, etc.
     c = csv.reader(f, delimiter=' ', skipinitialspace=True)
     for line in c:
         if line != []:
-            if line[-1] == 'milli-seconds.':
-                det.append(['new_image'])
+            if line[-1] == 'milli-seconds.': # check if we have seen a new image
+                det.append(['new_image']) # mark we are looking at a new image
                 img.append(line)
             if line[0] == 'Bins:':
                 line[1] = line[1].split('\t')[0]
                 line[8] = line[8].split(')')[0]
-                line[2] = float(line[2])/float(640)
-                line[4] = float(line[4])/float(480)
-                line[6] = float(line[6])/float(640)
-                line[8] = float(line[8])/float(480)
+                line[2] = float(line[2])/float(640) # reformat the coordinates, widths, heights into ratio of the image
+                line[4] = float(line[4])/float(480) # reformat the coordinates, widths, heights into ratio of the image
+                line[6] = float(line[6])/float(640) # reformat the coordinates, widths, heights into ratio of the image
+                line[8] = float(line[8])/float(480) # reformat the coordinates, widths, heights into ratio of the image
                 det.append(line)
 # up till here, we have line[2] as top left corner's x coordinate,
 # line[4] as the top left corner's y coordinate,
@@ -26,9 +27,9 @@ with open("result_bins.txt") as f:
 # line[8] as the height,
 # all in ratio numbers
 
-for line in det:
+for line in det: # for each object detected
     if line[0] == 'Bins:':
-        line[2] = line[2]+line[6]/2
+        line[2] = line[2]+line[6]/2 # reformat the information into the same as the ground truth information
         line[4] = line[4]+line[8]/2
         line.remove(line[0])
         line.remove(line[0])
@@ -38,17 +39,17 @@ for line in det:
 
 
 gro = []
-for file in os.listdir("C:/Users/xingz/OneDrive/Desktop/test_bin"):
+for file in os.listdir("C:/Users/xingz/OneDrive/Desktop/test_bin"): # read in each txt file containing bounding box information for one image
     if file.endswith(".txt"):
-        gro.append(['new_image'])
+        gro.append(['new_image']) # mark when we see a new image
         with open(os.path.join("C:\\Users\\xingz\\OneDrive\\Desktop\\test_bin",file)) as f:
             c = csv.reader(f, delimiter=' ', skipinitialspace=True)
             for line in c:
-                gro.append(line[1:])
+                gro.append(line[1:]) # attach the new object information
 # now we have both the ground truth and the detections, in the same format
 
 gro_pro = []
-while ['new_image'] in gro:
+while ['new_image'] in gro: # reformat the information, the each sublist is an image, each subsublist is an object in the image
     gro.remove(gro[gro.index(['new_image'])])
     if ['new_image'] in gro:
         gro_pro.append(gro[0:gro.index(['new_image'])])
@@ -57,7 +58,7 @@ while ['new_image'] in gro:
         gro_pro.append(gro[0:])
 
 det_pro = []
-while ['new_image'] in det:
+while ['new_image'] in det: # reformat the information, the each sublist is an image, each subsublist is an object in the image
     det.remove(det[det.index(['new_image'])])
     if ['new_image'] in det:
         det_pro.append(det[0:det.index(['new_image'])])
@@ -72,12 +73,12 @@ while ['new_image'] in det:
 
 accurate_amount = 0
 total_amount = 200
-for i in range(0, total_amount):
+for i in range(0, total_amount): # loop through each image
     det_c = det_pro[i]
     gro_c = gro_pro[i]
      # number of objects in the prediction image
     if len(gro_c) == len(det_c):
-        accurate_amount = accurate_amount+1
+        accurate_amount = accurate_amount+1 # count accurate number of images
         
 print('The accuracy for the number of bins in the image is:')
 print(accurate_amount/total_amount*100, '%')
